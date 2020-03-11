abe3aba71b198c586d592cd08ad592f0a0eb39d3
N03432852 <gzod1016@gmail.com>
2020-03-11 16:40:37 -0400

    fixed it again

--- temp.py
+++ temp.py
@@ -8,7 +8,7 @@ tempPin = 17
 tempSensor = Adafruit_DHT.DHT11
 
 while True:
-        humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPIN)
+        humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
         temperature = temperature * 9/5.0 +32
         if humidity is not None and temperature is not None:
 #              tempFahr - '{0:0.1f}*F'.format(temperature)
